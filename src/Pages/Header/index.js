d8ea5a482660dca55fe042979a5ecf90b8fccc58
Yogesh <yogeshfunde5@gmail.com>
2025-04-17 13:15:05 +0530

    create header and get css

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -1,7 +1,13 @@
 const Header = () => {
   return (
     <>
-      <h1>Header Page</h1>
+      <div className="headerWrapper">
+        <div className="top-strip bg-blue">
+            <div className="container">
+                <p className="mb-0 mt-0 text-center"> Due to covid 19 epidemic, order may be proceed slight delay</p>
+            </div>
+        </div>
+      </div>
     </>
   );
 };
