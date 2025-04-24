ac9dc7429404f0a184d11471dd64867fbcb8e7e5
Yogesh <yogeshfunde5@gmail.com>
2025-04-24 12:16:27 +0530

    remove swiper from product and add banner in home content

--- src/Components/HomeCat/index.js
+++ src/Components/HomeCat/index.js
@@ -1,5 +1,52 @@
-const HomeCat = () =>{
-    return(<>
-    </>)
-}
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useState } from "react";
+
+const HomeCat = () => {
+  const [itemBg, setItemBg] = useState([
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#f2fce4",
+    "#feefea",
+    "#ecffec",
+  ]);
+  return (
+    <section className="homeCat">
+      <div className="container">
+        <h3 className="mb-3 hd">Feature Categories</h3>
+        <Swiper
+          slidesPerView={10}
+          spaceBetween={8}
+          slidesPerGroup={1}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {itemBg?.map((item, index) => {
+            return (
+              <SwiperSlide>
+                <div className="item text-center cursor" style={{background:item}}>
+                  <img
+                    src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-53-346x310.jpg"
+                    alt=""
+                  />
+                  <h4>Fresh Produce Melons Each</h4>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
 export default HomeCat;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,30 +1,31 @@
 import HomeBanner from "../../Components/HomeBanner";
+import HomeCat from "../../Components/HomeCat";
 import banner1 from "../../assets/images/banner1.jpg";
 import banner2 from "../../assets/images/banner2.jpg";
 import Button from "@mui/material/Button";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
 import ProductItem from "../../Components/ProductsItem";
+import banner3 from "../../assets/images/banner3.jpg";
+import banner4 from "../../assets/images/banner4.jpg";
 
 const Home = () => {
   return (
     <>
       <HomeBanner />
-      <section className="homeCat"><div className="container">
-        </div></section>
+      <HomeCat />
       <section className="homeProducts">
         <div className="container">
           <div className="row">
             <div className="col-md-3">
-              <div className="banner">
-                <img src={banner1} className="cursor w-100" alt="" />
-              </div>
-              <div className="banner mt-4">
-                <img src={banner2} className="cursor w-100" alt="" />
+              <div className="sticky">
+                <div className="banner">
+                  <img src={banner1} className="cursor w-100" alt="" />
+                </div>
+                <div className="banner mt-4">
+                  <img src={banner2} className="cursor w-100" alt="" />
+                </div>
               </div>
             </div>
             <div className="col-md-9 ptoductRow">
@@ -39,29 +40,23 @@ const Home = () => {
                   View all <IoIosArrowRoundForward />
                 </Button>
               </div>
-              <div className="product_row w-100 mt-4">
-                <Swiper
-                  slidesPerView={4}
-                  spaceBetween={0}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  modules={[Navigation]}
-                  className="mySwiper"
-                >
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-                </Swiper>
+              <div className="product_row productrow2 w-100 mt-4 d-flex">
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+              </div>
+              <div className="d-flex">
+                <div className="banner mt-4 mb-5 bannerSec">
+                  <img src={banner3} className="cursor w-100" alt="" />
+                </div>
+                <div className="banner mt-4">
+                  <img src={banner4} className="cursor w-100" alt="" />
+                </div>
               </div>
             </div>
           </div>
